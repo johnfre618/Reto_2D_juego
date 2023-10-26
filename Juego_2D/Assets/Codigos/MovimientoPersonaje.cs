91cd4b3773dd6788199b0830dda94f6156783032
johnfre618 <johnfre.618@hotmail.com>
2023-10-25 19:45:13 -0500

    creacion nivel 2

--- Juego_2D/Assets/Codigos/MovimientoPersonaje.cs
+++ Juego_2D/Assets/Codigos/MovimientoPersonaje.cs
@@ -8,14 +8,15 @@ public class MovimientoPersonaje : MonoBehaviour
     public float fuerzaSalto = 7f;
 
     public Vector3 posicionInicio { get; set; }
+    public Vector3 escalaInicio { get; set; }
 
     private bool enElsuelo = false;
     private Rigidbody2D cuerpoRigido;
     private Animator animaciones;
 
-    
     void Awake()
     {
+        escalaInicio = transform.localScale;
         posicionInicio = transform.position;
         cuerpoRigido = GetComponent<Rigidbody2D>();   
         animaciones = GetComponent<Animator>();
@@ -24,18 +25,20 @@ public class MovimientoPersonaje : MonoBehaviour
     void Update()
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
+
         cuerpoRigido.velocity = new Vector2(movimientoHorizontal * velocidadMovimiento, cuerpoRigido.velocity.y);
 
         if (Input.GetButtonDown("Jump") && enElsuelo)
         {
+            cuerpoRigido.velocity = new Vector2(movimientoHorizontal * velocidadMovimiento,0);
             cuerpoRigido.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
             enElsuelo = false;
         }
 
         if (movimientoHorizontal > 0)
-            transform.localScale = new Vector3(2, 2, 2);
+            transform.localScale = new Vector3(escalaInicio.x, escalaInicio.y, escalaInicio.z);
         else if (movimientoHorizontal < 0)
-            transform.localScale = new Vector3(-2, 2, 2);
+            transform.localScale = new Vector3(-escalaInicio.x, escalaInicio.y, escalaInicio.z);
 
         animaciones.SetInteger("Salto", (int) cuerpoRigido.velocity.y);
         animaciones.SetBool("Piso", enElsuelo);
